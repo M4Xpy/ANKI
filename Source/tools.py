e33a43b8a40301825948a1b1734d6f3b025caaff
Maxim Voytkevychus <maxim.mixam@meta.ua>
2023-05-11 17:04:45 +0300

    in generate_audio_file refactor folfer from str to tuple

--- Source/tools.py
+++ Source/tools.py
@@ -41,12 +41,12 @@ def detect_language(text: str) -> str:
     raise ValueError('Input text cannot be empty.')
 
 
-def generate_audio_file(input_string: str, save_file: Optional[int] = 0) -> Optional[str]:
+def generate_audio_file(text: str, save_file: Optional[int] = 0) -> Optional[str]:
     """Generates audio file of the input_string in its detected language."""
-    folder = 'C:\\Users\\Я\\Desktop\\audio'  # default directory
-    lang = detect_language(input_string)  # Detect language of the input_string
-    audio_file_name = uniq_name(input_string) + '.mp3'  # Generate audio file name
-    audio = gTTS(text=input_string, lang=lang, slow=False)  # Generate audio file
+    folder = ('C:\\Users\\Я\\Desktop\\audio', f"C:\\Users\\Я\\Documents\\Anki\\1-й пользователь\\collection.media")[save_file]
+    lang = detect_language(text)  # Detect language of the input_string
+    audio_file_name = uniq_name(text) + '.mp3'  # Generate audio file name
+    audio = gTTS(text=text, lang=lang, slow=False)  # Generate audio file
     audio_file_path = os.path.join(folder, audio_file_name)  # Save audio file to directory
     audio.save(audio_file_path)
     if not save_file:
