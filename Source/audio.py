2f5614340ed48de7e674f9d856e04a2e8fb2fbd2
Maxim Voytkevychus <maxim.mixam@meta.ua>
2023-05-12 11:36:55 +0300

    temlate for anki cart maker for ChutGpt

--- Source/audio.py
+++ Source/audio.py
@@ -1,40 +1,8 @@
-def star_separated_words_from(text: str) -> str:
-    """ extract first word of each line, removing any digits or underscores from the word, and join them with asterisks
-    >>> star_separated_words_from('one , two\\nthree , four\\nfive , six')
-    ' * one * three * five * '
-    """
-    return f" * {' * '.join(line.split()[0].strip('_1234567890') for line in text.splitlines() if line and '.mp3' not in line)} * "
+import keyboard as keyboard
+
+while True:
+    if keyboard.is_pressed("ctrl + 0"):
+        print(111)
 
 
-print(("first , second , \\n\\n , third , fourth "))
-print(star_separated_words_from("""
-BACKING_3667 _поддержка, задний ход, подкладка, опора, подложка  
 
-BACKLOG   задолженность, резервы , невыполненные заказы
-
-BACKUP_4582 _резервный, резервное копирование, резервирование, создавать резервную копию, дублирование, дублирующий
-
-BACKWARDS_3784 _в обратном направлении
-
-COMEBACK_6663 _возвращение
-
-CUTBACK_7926 _сокращение
-
-OUTBACK_12203 _малонаселенная, необжитая местность
-
-SETBACK_8783 _регресс , неудача , задержка
-
-
-ABACK_10140 _назад, задом, сзади
-
-BACK   назад , обратно , защитник , спина
-
-backfire  обратный эффект , встречный пожар , разрыв патрона
-
-backfired  имел неприятные последствия
-
-[sound:BACKING.mp3][sound:BACKLOG.mp3][sound:BACKUP.mp3][sound:BACKWARDS.mp3][sound:COMEBACK.mp3][sound:CUTBACK.mp3][sound:OUTBACK.mp3][sound:SETBACK.mp3][sound:ABACK.mp3][sound:BACK.mp3]
-
-[sound:backfire.mp3]
-
-"""))
---
+++ Source/templates.py
@@ -0,0 +1,28 @@
+ai = f"""
+        It is important that the template of your answer matches the following example.
+        I ask you for a word 'Linger'.
+        You answer me strictly in the following format.
+        Beginning of the my example.
+
+        Linger - медлить, затягиваться
+        Lingers - затягивается (third person singular present tense)
+        Lingered - затянулся (past tense)
+        Lingering - затяжной, медленный (present participle)
+        Lingerer - медлитель (noun)
+        Lingerie - женское белье (noun)
+
+        "Time flies over us, but leaves its shadow behind."
+        "Время летит над нами, но оставляет свой след" (Nathaniel Hawthorne).
+
+        "Don't linger too long in the past, or you'll risk missing the present."
+        "Не задерживайся слишком долго в прошлом, иначе рискуешь упустить настоящее."
+
+        "The taste of success lingers long after the work is done."
+        "Вкус успеха остается долго после того, как работа закончена."
+
+        "Time and tide wait for no man."
+        "Время и прилив не ждут никого" (Geoffrey Chaucer).
+
+        End of the my example.
+        Now give me single-root words and forms for the word '{in_text}' with theirs translate on russian .And proverbs with some of these words with translate on russian.
+        """
