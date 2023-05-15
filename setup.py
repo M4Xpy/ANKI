573130c03a04d2e1dd887880ccb07ef0a997815f
Maxim Voytkevychus <maxim.mixam@meta.ua>
2023-05-15 18:57:41 +0300

    add requirements.txt, setup.py and LICENCE

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='ANKI',
+    version='1.1',
+    packages=['tests', 'Source'],
+    url='',
+    license='none',
+    author='Maxim Voytkevychus',
+    author_email='m4k51m15k4m@gmail.com',
+    description='work with Anki cards , etc'
+)
