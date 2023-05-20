f4ed2200a4637ab491810efb8e5cd046e26290d9
Maxim Voytkevychus <maxim.mixam@meta.ua>
2023-05-20 13:00:51 +0300

    add pytest and some corrects to star_separated_words_from()

--- Source/tools.py
+++ Source/tools.py
@@ -117,12 +117,13 @@ def star_separated_words_from(text: str) -> str:
     >>> star_separated_words_from('one , two\\n\\nthree , four\\nfive , six')
     ' * one * three * five * '
     >>> star_separated_words_from(' * MENACING * [sound:Menacer.mp3]')
-    '* MENACING *'
+    ' * MENACING * '
     """
     lines = [line.split()[0].strip('_1234567890') for line in text.splitlines() if line and '.mp3' not in line]
     if len(lines) < 2:
-        return ' '.join(word for word in text.split() if '[sound:' not in word)
+        return f" * {' * '.join(word for word in text.split() if '*' not in word and '.mp3' not in word)} * "
     return f" * {' * '.join(lines)} * "
+
 
 
 def header_tab_mp3() -> None:
