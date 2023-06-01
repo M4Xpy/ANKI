2c97b22dc9c4b08d97915acf2577b3617cf1fd41
Maxim Voytkevychus <maxim.mixam@meta.ua>
2023-06-01 17:39:00 +0300

    init

--- Source/mp3name_detector.py
+++ Source/mp3name_detector.py
@@ -1,41 +1,55 @@
-file_path = 'E:\\test_txt.txt'
+import re
 
-def find_words_with_letter_combinations(file_path: str) -> set[str]:
+file_path = 'E:\\test_txt.txt', 'C:\\Users\\Я\\Desktop\\D  .    3   .   1.txt', 'C:\\Users\\Я\\Desktop\\Все колоды.txt', \
+            'C:\\Users\\Я\\Desktop\PythonProjectsFrom22_04_2023\\ANKI\\additional_data\\Карточки в простой текст.txt'
+
+
+def find_mp3_resfers(path_of_file: str) -> set[str]:
+    # """ find mp3 refers from *.txt anki file
+    # >>> sorted(find_mp3_resfers(file_path[-1]))
+    # ['ALCOVE.mp3', 'COVE.mp3', 'COVENANT.mp3', 'COVERT.mp3', 'COVET.mp3', 'OVERT.mp3', 'TENABLE.mp3']
+    # """
     words = set()
-    ommit = True
-    with open(file_path, encoding="utf-8") as file:
+    omit = True
+    with open(path_of_file, encoding="utf-8") as file:
         for part in file:
             for letter in part:
-                if ommit:
+                if omit:
                     if letter == '[':
-                        ommit = False
+                        omit = False
                         word = '['
                 else:
                     word = f'{word}{letter}'
                     if letter == ']':
-                        ommit = True
-                        words.add(word)
+                        omit = True
+                        words.add(word.strip('[sound:]'))
     return words
 
 
-
-
-text = """word = word.strip(' _1234567890')
-translations = translations_of_the(word)
-result = f" * {' * '.join(word for word in translations)} * "
-return result"""
-
-
-def one_line_code_compressor(code: str):
-    """ Make one-line code from many-line code
-    >>> print(one_line_code_compressor(text))
-    return f" * {' * '.join(word.strip(' _1234567890') for word in translations_of_the(word))} * "
+def find_pictuare_refers(path_of_file: str) -> set[str]:
+    """ find pictuare refers from *.txt anki file
+    >>> find_pictuare_refers(file_path[-1])
     """
-    result = ''
-    for line in reversed(code.splitlines()):
-        if ' = ' in line:
-            result = result.replace(*line.split(' = '), 1)
-        else:
-            result = f"{line}{result}"
-    return result
+    pictuares = set()
+    text = '<img src=""paste-'
+    pictuare = ''
+    with open(path_of_file, encoding="utf-8") as file:
+        content = file.read()
+        res = re.findall("<img src=.{29} />", content)
+        while res:
+            res = res[0]
+            pictuares.add(res)
+            content = content.replace(res, '')
+            res = re.findall("<img src=.{29} />", content)
 
+        # for part in file:
+        #     for letter in part:
+        #         if not pictuare:
+        #             if text[-17:] == text:
+        #                 pictuare = f'{pictuare}{letter}'
+        #         else:
+        #             if pictuare.count('"') > 2:
+        #                 pictuares.add(pictuare)
+        #             else:
+        #                 pictuare = f'{pictuare}{letter}'
+    return pictuares
