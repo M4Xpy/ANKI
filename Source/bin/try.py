a4b6279790cda8b18290d64c0b52697dcb2792c5
Maxim Voytkevychus <maxim.mixam@meta.ua>
2023-06-26 14:55:21 +0300

    delete lazy_mouse() command

--- Source/bin/try.py
+++ Source/bin/try.py
@@ -10,12 +10,12 @@ def add_prefix_to_files(folder_path):
             new_filename = f'{filename.replace("(", "").replace(")", "")}'
             os.rename(os.path.join(folder_path, filename), os.path.join(folder_path, new_filename))
 
+
 # Example usage
 folder_path = f"C:\\ANKIsentences\\triples_audio\\15\\10"
+
 
 def lazy_mouse():
     while True:
         keyboard.press('down')
         time.sleep(10)
-
-lazy_mouse()
