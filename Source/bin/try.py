a3515db852f45adf0bbfe8bb852fb4190e06467e
Maxim Voytkevychus <maxim.mixam@meta.ua>
2023-07-06 11:25:27 +0300

    recite_subtitle_from_video.py now async in try.py

--- Source/bin/try.py
+++ Source/bin/try.py
@@ -1,9 +1,79 @@
+import asyncio as asyncio
+import os
+from difflib import SequenceMatcher
+
+import cv2
+import keyboard as keyboard
+import np as np
+import pyautogui
+import pygame
+import pytesseract
+from googletrans import Translator
+from gtts import gTTS
+
+# from pydub import AudioSegment
+# from pydub.playback import play
+#
+#
+# def play_audio_with_adjustments(audio_path, speed_factor, volume_adjustment):
+#     # Load the audio file
+#     audio = AudioSegment.from_file(audio_path)
+#
+#     # Adjust the speed
+#     modified_audio = audio.speedup(playback_speed=speed_factor)
+#
+#     # Adjust the volume
+#     modified_audio = modified_audio + volume_adjustment
+#
+#     # Play the modified audio
+#     play(modified_audio)
 
 
-def funk0():
-    text = funk1()
-    if text:
-        replaced = funk2()
-        text.replace(replaced)
+new = '111111111111111111111'
+old = '9999999999999999999999'
 
-# refine funk0() to async , line 'if text:' and 'funk2()' must operating simultainously , if 'if text:' == False second thread aborted
+
+async def text_not_old(text):
+    return text and SequenceMatcher(None, old, text).ratio() < 0.5
+
+
+async def translate_and_make_audio(text):
+    translated = gTTS(text=Translator().translate(text, 'uk', 'en').text, lang='uk')
+    translated.save("C:\\ANKIsentences\\temporary.mp3")
+
+
+async def extract_clear_text_with_thresholding():
+    global old, new
+    pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe '
+    screenshot = pyautogui.screenshot()
+    width, height = screenshot.size
+    text = pytesseract.image_to_string(
+            cv2.threshold(
+                    cv2.cvtColor(
+                            cv2.cvtColor(
+                                    np.array(screenshot.crop((0, 0, width, int(height * 0.95)))), cv2.COLOR_RGB2BGR
+                                    ),
+                            cv2.COLOR_BGR2GRAY
+                            ), 0, 255, cv2.THRESH_BINARY_INV
+                    )[1]
+            ).splitlines()
+    text = ' '.join(line.replace('|', 'I') for line in text if len(line.replace(' .,_-|\'§—012345678"9=+*/°[]()~@#', '')) > 9)
+    if await asyncio.create_task(text_not_old(text)):  # and SequenceMatcher(None, new, text).ratio() < 0.8
+        keyboard.press('space')
+        await asyncio.create_task(translate_and_make_audio(text))
+        pygame.mixer.init()
+        pygame.mixer.music.load("C:\\ANKIsentences\\temporary.mp3")
+        pygame.mixer.music.play()
+        while pygame.mixer.music.get_busy():
+            continue
+        pygame.mixer.quit()
+        os.remove("C:\\ANKIsentences\\temporary.mp3")
+        old = text
+        keyboard.press('space')
+        print(text)
+
+
+exceptions = ('mm SB HB oo. E\n\n- You got any kids?',)
+if __name__ == '__main__':
+    while True:
+        asyncio.run(extract_clear_text_with_thresholding())
--- Source/recite_subtitle_from_video.py
+++ Source/recite_subtitle_from_video.py
@@ -1,4 +1,5 @@
 import os
+import re
 from difflib import SequenceMatcher
 
 import cv2
@@ -9,27 +10,12 @@ import pyautogui
 import pytesseract
 from googletrans import Translator
 from gtts import gTTS
-# from pydub import AudioSegment
 
-
-# def play_audio_with_speed(audio_path):
-#     audio = AudioSegment.from_file(audio_path)
-#     new_audio = audio.speedup(playback_speed=2)
-#     modified_audio_path = "C:\\Users\\Я\\Desktop\\PythonProjectsFrom22_04_2023\\ANKI\\additional_data\\mp3s_for_tests\\modified_audio.mp3"
-#     new_audio.export(modified_audio_path, format="mp3")
-#
-#     playsound.playsound(modified_audio_path)
-#
-#     # Delete the modified audio file
-#     os.remove(modified_audio_path)
-
-
-new = '111111111111111111111'
-old = '9999999999999999999999'
+old = 'aaaa'
 
 
 def extract_clear_text_with_thresholding():
-    global old, new
+    global old
     pytesseract.pytesseract.tesseract_cmd = 'C:\\Program Files\\Tesseract-OCR\\tesseract.exe'
     screenshot = pyautogui.screenshot()
     width, height = screenshot.size
@@ -42,18 +28,17 @@ def extract_clear_text_with_thresholding():
                             cv2.COLOR_BGR2GRAY
                             ), 0, 255, cv2.THRESH_BINARY_INV
                     )[1]
-            ).replace('|', 'I').splitlines() # strip(' .,_-|§—')
-    text = ' '.join(line for line in text if len(line) > 4)
-    if text and SequenceMatcher(None, old, text).ratio() < 0.8:  # and SequenceMatcher(None, new, text).ratio() < 0.8
-        audio_file_path = 'C:\\Users\\Я\\Desktop\\PythonProjectsFrom22_04_2023\\ANKI\\additional_data\\mp3s_for_tests\\temporary.mp3'
-        translated = gTTS(text=Translator().translate(text, 'ru', 'en').text, lang='ru')
+            ).replace('|', 'I').splitlines()
+    text = ' '.join(line for line in text if len(re.sub('[^A-z]', '', '_ rT . i -')) > 4)
+    if text and SequenceMatcher(None, old, text).ratio() < 0.8:
+        gTTS(text=Translator().translate(text, 'ru', 'en').text, lang='ru').save("C:\\ANKIsentences\\temporary.mp3")
         keyboard.press('space')
-        translated.save(audio_file_path)
-        playsound.playsound(audio_file_path)
-        os.remove(audio_file_path)
-        old = text  # old, new = new, text
+        playsound.playsound("C:\\ANKIsentences\\temporary.mp3", block=True)
+        keyboard.press('space')
+        os.remove("C:\\ANKIsentences\\temporary.mp3")
+        old = text
         print((text,))
-        keyboard.press('space')
+
 
 exceptions = ('mm SB HB oo. E\n\n- You got any kids?',)
 if __name__ == '__main__':
